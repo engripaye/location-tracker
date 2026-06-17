a0ebe0e84df4c2c71a640ed824fb3c74a00287f7
mac <@b.tunde.ipaye@gmail.com>
2026-06-17 05:51:46 +0100

    backend code commit by ENGR. IPAYE

--- app/Security.py
+++ app/Security.py
@@ -8,3 +8,16 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHMS = os.getenv("ALGORITHMS")
 
+def hash_password(password):
+    return pwd_context.hash(password)
+
+def verify_password(
+        plain_password,
+        hashed_password): return pwd_context.verify(plain_password, hashed_password)
+
+def create_access_token(
+        data: dict
+):
+    payload = data.copy()
+
+    payload["exp"] = datetime.utcnow() + timedelta(hours=1)
