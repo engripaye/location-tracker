4c62648cae4ed6542ee2eb7a911946e4d7c3a393
mac <@b.tunde.ipaye@gmail.com>
2026-06-17 06:25:53 +0100

    backend code commit by ENGR. IPAYE

--- routes/tracking.py
+++ routes/tracking.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+from pyasn1 import error
 from sqlalchemy import FromClause
 from sqlalchemy.orm import Session
 from fastapi import Depends
@@ -32,3 +33,37 @@ def save_location(session_id: int, request: LocationRequest, db: Session = Depen
     db.commit()
 
     return {"message": "saved"}
+
+@router.get("/{share_token}")
+def latest_location(
+        share_token: str,
+        db: Session = Depends(get_db),
+):
+    session: db_query(
+        TrackingSession,
+    ).filter(
+        TrackingSession.share_token == share_token
+    ).first()
+
+    if not session:
+        return {"error": "Not found"}
+
+    location = db.query(LocationPoint).filter(
+        LocationPoint.session_id == session.id
+    ).order_by(
+        LocationPoint.created_at.desc()
+    ).first()
+
+    if not location:
+        return {"error": "No Location"}
+
+    return {
+        "latitude":
+            location.latitude,
+        "longitude":
+            location.longitude,
+        "accuracy":
+            location.accuracy,
+        "timestamp":
+            location.created_at
+    }
