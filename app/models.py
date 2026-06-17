25dfacca1fbe96cadcdc322bb7ce7b6829ee67d5
mac <@b.tunde.ipaye@gmail.com>
2026-06-17 05:38:57 +0100

    backend code commit by ENGR. IPAYE

--- app/models.py
+++ app/models.py
@@ -21,4 +21,12 @@ class User(Base):
     email = Column(String)
     password_hash = Column(String)
 
+    class TrackingSession(Base):
+        __tablename__ = 'tracking_session'
+        id = Column(Integer, primary_key=True)
 
+        owner_id = Column(Integer, ForeignKey('user.id'))
+
+        share_token = Column(String, unique=True)
+
+        active = Column(Boolean, default=True)
