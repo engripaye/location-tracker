15164982f72b4bfbbce41fbd9664bec8d752b94b
mac <@b.tunde.ipaye@gmail.com>
2026-06-17 05:42:11 +0100

    backend code commit by ENGR. IPAYE

--- app/models.py
+++ app/models.py
@@ -14,19 +14,32 @@ from datetime import datetime
 from app.database import Base
 
 class User(Base):
-    __tablename__ = 'user'
+    __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
     name = Column(String)
     email = Column(String)
     password_hash = Column(String)
 
-    class TrackingSession(Base):
-        __tablename__ = 'tracking_session'
-        id = Column(Integer, primary_key=True)
+class TrackingSession(Base):
+    __tablename__ = 'tracking_sessions'
+    id = Column(Integer, primary_key=True)
 
-        owner_id = Column(Integer, ForeignKey('user.id'))
+    owner_id = Column(Integer, ForeignKey('user.id'))
 
-        share_token = Column(String, unique=True)
+    share_token = Column(String, unique=True)
 
-        active = Column(Boolean, default=True)
+    active = Column(Boolean, default=True)
+
+    created_at = Column(DateTime, default=datetime.now())
+
+
+class LocationPoint(Base):
+    __tablename__ = 'location_points'
+    id = Column(Integer, primary_key=True)
+
+    session_id = Column(Integer, ForeignKey('tracking_sessions.id'))
+
+    latitude = Column(Float)
+    longitude = Column(Float)
+
