5b38e415b2add571327d6369a2d80916cfdb1e83
mac <@b.tunde.ipaye@gmail.com>
2026-06-17 05:29:52 +0100

    backend code commit by ENGR. IPAYE

--- app/database.py
+++ app/database.py
@@ -4,3 +4,12 @@ from dotenv import load_dotenv
 import os
 
 
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
